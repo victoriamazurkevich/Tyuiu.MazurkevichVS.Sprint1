931e879b928875992f13c62a41566fcbc9d0b920
Виктория Мазуркевич <victoria090707maz@gmail.com>
2025-09-27 21:13:02 +0500

    Task5

--- Tyuiu.MazurkevichVS.Sprint1.Task5.V1.Lib/DataService.cs
+++ Tyuiu.MazurkevichVS.Sprint1.Task5.V1.Lib/DataService.cs
@@ -1,11 +1,13 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint1;
+﻿using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.MazurkevichVS.Sprint1.Task5.V1.Lib
 {
     public class DataService : ISprint1Task5V1
     {
         public int DistanceBetweenDots(double x1, double y1, double x2, double y2)
         {
-            return (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-        }
+            double res = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            return (int)Math.Round(res);
+        }   
     }
 }
